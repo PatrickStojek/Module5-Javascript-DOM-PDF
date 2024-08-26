56b51cc178aae2b4fbda30deefa229b39d8c0e4e
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-26 17:05:22 +0200

    add if statment to improve code performance

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const fooEl = document.querySelector('#foo')
 if(fooEl) {
     const boxList = document.getElementsByClassName('box')
-    for(let i = 0; i < boxList.length; i++) {
-        const pList = boxList[i].querySelectorAll('p')
-        pList.forEach(element => {
-                element.style.border = '2px solid green'
-        });
+    if(boxList.length >  0) {
+        for(let i = 0; i < boxList.length; i++) {
+            const pList = boxList[i].querySelectorAll('p')
+            pList.forEach(element => {
+                    element.style.border = '2px solid green'
+            });
+        }
     }
+    
 }
